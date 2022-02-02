9a77281b3d4e0187a1251d2260e081e9c9440210
Rajitha Fernando <chandimalfdo23@gmail.com>
2022-02-02 13:32:11 +0530

    version update with functional component

--- src/lib/CommentCount.jsx
+++ src/lib/CommentCount.jsx
@@ -2,31 +2,31 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { addScript, removeScript } from './util'
 
-export default class CommentCount extends React.Component {
-  setVariables() {
-    if (this.props.websiteId) {
-      window.HYVOR_TALK_WEBSITE = this.props.websiteId
-    }
-  }
-
-  componentDidMount() {
-    this.setVariables()
-
-    var scriptId = 'ht-comment-count-script'
-    if (document.getElementById(scriptId)) {
-      removeScript(scriptId)
+export default function CommentCount(props){
+    function setVariables(){
+        if (this.props.websiteId) {
+            window.HYVOR_TALK_WEBSITE = props.websiteId
+        }
     }
 
-    addScript('//talk.hyvor.com/web-api/count/', scriptId)
-  }
+    useEffect(() => {
+        setVariables();
 
-  render() {
-    return <span data-talk-id={this.props.id} mode={this.props.mode || 'default'} />
-  }
+        var scriptId = 'ht-comment-count-script'
+        if (document.getElementById(scriptId)) {
+            removeScript(scriptId)
+        }
+
+        addScript('//talk.hyvor.com/web-api/count/', scriptId)
+    });
+
+    return (
+        <span data-talk-id={this.props.id} mode={this.props.mode || 'default'} />
+    );
 }
 
 CommentCount.propTypes = {
-  id: PropTypes.any,
-  mode: PropTypes.any,
-  websiteId: PropTypes.number
+    id: PropTypes.any,
+    mode: PropTypes.any,
+    websiteId: PropTypes.number
 }
--- src/lib/Embed.jsx
+++ src/lib/Embed.jsx
@@ -5,44 +5,39 @@ import { addScript, removeScript } from './util'
 /**
  * Embeds Hyvor Talk thread on a web page
  */
-export default class Embed extends React.Component {
-  setVariables() {
-    window.HYVOR_TALK_WEBSITE = this.props.websiteId
-    window.HYVOR_TALK_CONFIG = {
-      url: this.props.url || false,
-      id: this.props.id || false,
-      title: this.props.title || document.title,
-      loadMode: this.props.loadMode || 'default',
-      language: this.props.language || null,
-      sso: this.props.sso || null,
-      palette: this.props.palette || null
+export default function Embed(props){
+    function setVariables() {
+        window.HYVOR_TALK_WEBSITE = props.websiteId
+        window.HYVOR_TALK_CONFIG = {
+            url: props.url || false,
+            id: props.id || false,
+            title: props.title || document.title,
+            loadMode: props.loadMode || 'default',
+            language: props.language || null,
+            sso: props.sso || null,
+            palette: props.palette || null
+        }
     }
-  }
 
-  addScript() {
-    if (document.getElementById('ht-embed-script')) {
-      removeScript('ht-embed-script')
-    }
-    addScript('//talk.hyvor.com/web-api/embed', 'ht-embed-script')
-  }
+    useEffect(() => {
+        setVariables();
 
-  componentDidMount() {
-    this.setVariables()
-    this.addScript()
-  }
+        if (document.getElementById('ht-embed-script')) {
+            removeScript('ht-embed-script')
+        }
+        addScript('//talk.hyvor.com/web-api/embed', 'ht-embed-script')
+    });
 
-  render() {
-    return <div id='hyvor-talk-view' />
-  }
+    return (<div id='hyvor-talk-view' />);
 }
 
 Embed.propTypes = {
-  websiteId: PropTypes.number.isRequired,
-  url: PropTypes.any,
-  id: PropTypes.any,
-  title: PropTypes.string,
-  loadMode: PropTypes.any,
-  language: PropTypes.string,
-  sso: PropTypes.object,
-  palette: PropTypes.object
+    websiteId: PropTypes.number.isRequired,
+    url: PropTypes.any,
+    id: PropTypes.any,
+    title: PropTypes.string,
+    loadMode: PropTypes.any,
+    language: PropTypes.string,
+    sso: PropTypes.object,
+    palette: PropTypes.object
 }
